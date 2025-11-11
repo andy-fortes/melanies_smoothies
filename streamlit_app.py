5b4871243ef8c048a7a4f945fb63cf27bdc68b00
andy-fortes <andy.marland@fortescue.com>
2025-11-11 10:12:26 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import pandas as pd
 import requests
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
 st.write(
@@ -17,8 +19,14 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
+
 
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:'
@@ -32,14 +40,19 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-    #st.write(ingredients_string)
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME']== fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The Search value for ', fruit_chosen, ' is ', search_on, '.')
+
+        st.subheader(fruit_chosen + ' Nutritional Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 
     my_insert_stmt = """ insert into smoothies.public.orders (name_on_order,ingredients)
             values ('""" + name_on_order+ """','""" + ingredients_string + """')"""
 
+
+    
+    
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
@@ -47,7 +60,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success( name_on_order + ' '+ ' Your Smoothie is orderd!')
-
 
 
 
